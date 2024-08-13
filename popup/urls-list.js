d482459cc53c56914c6cb4860f7daa1942926064
Dachi Wolf <78776000+SexyWerewolf@users.noreply.github.com>
2024-08-13 06:09:21 +0300

    Update urls-list.js

--- popup/urls-list.js
+++ popup/urls-list.js
@@ -8,38 +8,40 @@ let resetFilterBtn = document.querySelector('.resetFilter');
 let urlText = document.querySelector('.urlText');
 let filterInput = document.querySelector('.filterInput');
 let filterWarning = document.querySelector('.filterWarning');
-
 let alwaysOpenAllTabs = false;
-browser.storage.sync.get().then(settings => {
-  alwaysOpenAllTabs = ('openUrlsAlreadyOpened' in settings) ? settings.openUrlsAlreadyOpened : false;
-}, error => {
-  console.log(`Error: ${error}`);
-});
-
+let filterBackup = '';
+let filterMode = false;
+function saveFilterValue(value) {
+  browser.storage.sync.set({ filterInputValue: value });
+}
+function restoreFilterValue() {
+  return browser.storage.sync.get('filterInputValue').then(settings => {
+    return ('filterInputValue' in settings) ? settings.filterInputValue : '';
+  });
+}
 function listTabs() {
   disableFilterMode();
-
   browser.tabs.query({currentWindow: true}).then((tabs) => {
     let urls = '';
     for (let tab of tabs) {
       urls += tab.url + '\n';
     }
     urlText.value = urls;
+    restoreFilterValue().then(value => {
+      filterInput.value = value;
+      filter();
+    });
   });
 }
-
 function open() {
   browser.tabs.query({currentWindow: true}).then((tabs) => {
-    // save list of current urls
     let currentUrls = [];
     for (let tab of tabs) {
       currentUrls.push(tab.url);
     }
     let newUrls = urlText.value.split('\n');
     for (let url of newUrls) {
-      // only open if new url is not empty string and is not already opened
       if (url !== '' && (alwaysOpenAllTabs || currentUrls.indexOf(url) < 0)) {
-        // prefix "http://" if it is not an url already
         if (url.indexOf('://') < 0) {
           url = 'http://' + url;
         }
@@ -51,17 +53,13 @@ function open() {
     }
   });
 }
-
 function copy() {
   let tmp = urlText.value;
   urlText.select();
   document.execCommand('Copy');
-
-  // workaround to not have text selected after button click
   urlText.value = '';
   urlText.value = tmp;
 }
-
 function save() {
   let d = new Date();
   let year = d.getFullYear();
@@ -71,19 +69,16 @@ function save() {
   let min = ('0' + d.getMinutes()).slice(-2);
   let sec = ('0' + d.getSeconds()).slice(-2);
   let dateString = [year, month, day, hour, min, sec].join('-');
-
   let dl = document.createElement('a');
-
-  dl.download = 'urls-list-' + dateString + '.txt'; // filename
+  dl.download = 'urls-list-' + dateString + '.txt';
   dl.href = window.URL.createObjectURL(
-    new Blob([urlText.value], {type: 'text/plain'}) // file content
+    new Blob([urlText.value], {type: 'text/plain'})
   );
   dl.onclick = event => document.body.removeChild(event.target);
   dl.style.display = 'none';
   document.body.appendChild(dl);
   dl.click();
 }
-
 function sort(desc = false) {
   let urls = urlText.value.split('\n');
   let cleanUrls = [];
@@ -99,28 +94,21 @@ function sort(desc = false) {
   }
   urlText.value = cleanUrls.join('\n') + '\n';
 }
-
 function sortAsc() {
   sort(false);
 }
-
 function sortDesc() {
   sort(true);
 }
-
-let filterBackup = '';
-let filterMode = false;
-
 function enableFilterMode() {
   if (!filterMode) {
     filterBackup = urlText.value;
     urlText.readOnly = true;
-    urlText.classList.add("urlTextFilterMode")
+    urlText.classList.add("urlTextFilterMode");
     filterWarning.classList.remove("hide");
     filterMode = true;
   }
 }
-
 function disableFilterMode() {
   if (filterMode) {
     urlText.value = filterBackup;
@@ -128,13 +116,11 @@ function disableFilterMode() {
     urlText.classList.remove("urlTextFilterMode");
     filterWarning.classList.add("hide");
     filterInput.classList.remove("filterInputError");
-    filterInput.value = '';
     filterMode = false;
   }
 }
-
 function filter(e) {
-  let val = e.target.value;
+  let val = e ? e.target.value : filterInput.value;
   filterInput.classList.remove("filterInputError");
   if (val !== '') {
     enableFilterMode();
@@ -155,12 +141,11 @@ function filter(e) {
   } else {
     disableFilterMode();
   }
+  saveFilterValue(val);
 }
-
 function resetFilter() {
   disableFilterMode();
 }
-
 document.addEventListener('DOMContentLoaded', listTabs);
 resetBtn.addEventListener('click', listTabs);
 openBtn.addEventListener('click', open);
